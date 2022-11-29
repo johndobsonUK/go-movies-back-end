a5352b01de356740dc33996d1cdcf322d78f9202
John Dobson <john@thedobsons.me.uk>
2022-11-29 14:29:08 +0000

    Implmented Postgres connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,19 +11,32 @@ import (
 const port = 8080
 
 type application struct {
+	DSN string
 	Domain string
+	DB *sql.DB
 }
 
 func main() {
 	// set application config
 	var app application
+
 	// read from the command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+
+	flag.Parse()
+
 	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+
 	app.Domain = "example.com"
 	// start a web server
 	log.Println("App is listening on port:", port)
 	
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
